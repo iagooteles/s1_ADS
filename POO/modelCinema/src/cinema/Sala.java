e5cdb653c5b66ccd830117112ed310c4ed8c7a3d
iagooteles <iagooteles@gmail.com>
2025-02-27 21:59:15 -0300

    teste

---
+++ POO/modelCinema/src/cinema/Sala.java
@@ -0,0 +1,6 @@
+package modelCinema.src.cinema;
+
+public class Sala {
+    public int id;
+    public int capacidade;
+}
---
+++ POO/modelCinema/src/cinema/Sessao.java
@@ -0,0 +1,10 @@
+package modelCinema.src.cinema;
+
+import java.time.LocalDateTime;
+
+public class Sessao {
+    public Filme filme;
+    public Sala sala;
+    public LocalDateTime horario;
+    public LocalDateTime dia;
+}
